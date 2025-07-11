9970b3ca4f40bd77b53f693de9548c2782d2796b
bumiayudev <husniarrasyid@gmail.com>
2025-07-11 20:08:11 +0700

    styling slide in home produk item

--- js/main.js
+++ js/main.js
@@ -12,33 +12,33 @@ mobileNav.addEventListener("click", () => toggleNav());
 
 // Slider functionality
 // Pastikan slider memiliki ID 'slider' dan setiap slide memiliki kelas 'slide'
-let currentIndex = 0;
+// let currentIndex = 0;
 
-function showSlide(index) {
-  const slider = document.getElementById('slider');
-  const slides = document.querySelectorAll('.slide');
-  const slideWidth = slides[0].offsetWidth + 20; // Tambah gap
-  const totalSlides = slides.length;
+// function showSlide(index) {
+//   const slider = document.getElementById('slider');
+//   const slides = document.querySelectorAll('.slide');
+//   const slideWidth = slides[0].offsetWidth + 20; // Tambah gap
+//   const totalSlides = slides.length;
 
-  if (index < 0) index = totalSlides - 1;
-  if (index >= totalSlides) index = 0;
+//   if (index < 0) index = totalSlides - 1;
+//   if (index >= totalSlides) index = 0;
 
-  currentIndex = index;
-  slider.style.transform = `translateX(-${slideWidth * index}px)`;
-}
+//   currentIndex = index;
+//   slider.style.transform = `translateX(-${slideWidth * index}px)`;
+// }
 
-function nextSlide() {
-  showSlide(currentIndex + 1);
-}
+// function nextSlide() {
+//   showSlide(currentIndex + 1);
+// }
 
-function prevSlide() {
-  showSlide(currentIndex - 1);
-}
+// function prevSlide() {
+//   showSlide(currentIndex - 1);
+// }
 
 // Auto-slide
-setInterval(() => {
-  nextSlide();
-}, 3000);
+// setInterval(() => {
+//   nextSlide();
+// }, 3000);
 
 // Modal functionality for gallery images
 // Menunggu hingga seluruh halaman dan elemennya siap
@@ -60,3 +60,30 @@ let date = new Date();
 let year = date.getFullYear();
 let footer = document.querySelector('.footer-bottom p');
 footer.innerHTML = `&copy; ${year} - All rights reserved`;
+
+const checkScrollForAnimation = () => {
+
+  let scrollY = this.window.scrollY || this.window.pageYOffset;
+  let productItem = this.document.querySelectorAll('.product-item');
+  let galleryItem = this.document.querySelectorAll('.gallery-item');
+  
+  if(scrollY >= 280){
+    productItem.forEach((card, index) =>{
+      setTimeout(()=>{
+        card.classList.add('slide-in');
+      }, index * 400)
+    });
+  }
+
+  if(scrollY >= 500){
+    galleryItem.forEach((card, index) => {
+      setTimeout(()=>{
+        card.classList.add('slide-in')
+      }, index * 400)
+    });
+    window.removeEventListener('scroll', checkScrollForAnimation);
+  }
+}
+
+window.addEventListener('scroll', checkScrollForAnimation);
+checkScrollForAnimation();
